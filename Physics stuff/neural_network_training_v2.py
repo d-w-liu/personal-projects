0fca800c37639869bda302ef11d210366594d08c
David Liu <dwliu@uw.edu>
2020-09-14 11:20:16 -0700

    apparently many changes

--- Physics stuff/neural_network_training_v2.py
+++ Physics stuff/neural_network_training_v2.py
@@ -1,7 +1,7 @@
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.svm import SVC
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 import pandas as pd
 import awkward
 # import servicex
@@ -14,7 +14,6 @@ from numpy import genfromtxt
 
 noise_events = 5000
 signal_events = 1500
-
 
 # Eventually, what I want here is to pull some dataset and train the neural network to separate noise from signal for two different variables.
 # But also, those two different variables need to remain uncorrelated.
@@ -39,29 +38,24 @@ signal_data_z = generate_distribution(events = signal_events, peak = 15.0, width
 noise_list = [0] * noise_events
 signal_list = [1] * signal_events
 
-var_x = noise_data_x[0] + signal_data_x[0]
-var_y = noise_data_y[0] + signal_data_y[0]
-var_z = noise_data_z[0] + signal_data_z[0]
-signal_or_noise = noise_list + signal_list
+var_x = noise_data_x + signal_data_x
+var_y = noise_data_y + signal_data_y
+var_z = noise_data_z + signal_data_z
+signal_or_noise_axis_1 = noise_list + signal_list
+signal_or_noise_axis_2 = noise_list + signal_list
 
-data_dict = {'var_1':var_x, 'var_2':var_y, 'var_3':var_z, 'sn':signal_or_noise}
+data_dict = {'var_1':var_x, 'var_2':var_y, 'var_3':var_z}
+output_dict = {'sn1':signal_or_noise_axis_1, 'sn2':signal_or_noise_axis_2}
 
 # Construct a pandas dataframe with all our data and prepare it for analysis:
 
-data = pd.DataFrame[data_dict]
-input_data = data.drop('sn', axis=1)
-output_data = data['sn']
+input_data = pd.DataFrame(data_dict)
+output_data = pd.DataFrame(output_dict)
 
 input_train, input_test, output_train, output_test = train_test_split(input_data, output_data)
 
-scaler = StandardScaler()
-
-scaler.fit(input_train)
-
-input_train = scaler.transform(input_train)
-input_test = scaler.transform(input_test)
-
-neural_network = MLPClassifier(hidden_layer_sizes=(50,50), activation='tanh', max_iter=500)
+svc = SVC(gamma="scale")
+neural_network = MultiOutputClassifier(estimator=svc)
 
 # Train the actual neural network
 
